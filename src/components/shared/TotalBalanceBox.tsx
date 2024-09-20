349c7d1153f2e7aba3d91d571e5124958be930ce
José Brito <josealbritose@gmail.com>
2024-09-20 17:49:56 -0300

    Fix styles - TotalBalanceBox

--- src/components/shared/TotalBalanceBox.tsx
+++ src/components/shared/TotalBalanceBox.tsx
@@ -1,26 +1,20 @@
-import { formatAmount } from '@/lib/utils'
-import React from 'react'
-import AnimatedCounter from './AnimatedCounter'
+import React from "react";
+import AnimatedCounter from "./AnimatedCounter";
+import DoughnutChart from "./DoughnutChart";
 
-export default function TotalBalanceBox({accounts=[],totalBanks,totalCurrentBalance}:TotalBalanceBoxProps) {
+export default function TotalBalanceBox({accounts = [],totalBanks,totalCurrentBalance,}: TotalBalanceBoxProps) {
   return (
-    <section>
-      <div className='total-balance-chart'>
-
+    <section className="total-balance">
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts}/>
       </div>
-
-      <div className='flex flex-col gap-6'>
+      <div className="flex flex-col gap-6">
         <h2>Bank Accounts: {totalBanks} </h2>
-        <div className='flex flex-col gap-2'>
-          <p className='total-balance-label'>Total Current Balance</p>
-          <p className='total-balance-amount flex-center gap-2'>
-            <AnimatedCounter amount={totalCurrentBalance}/>
-
-            {formatAmount(totalCurrentBalance)}
-          </p>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <div className="total-balance-amount flex-center gap-2"><AnimatedCounter amount={totalCurrentBalance} /></div>
         </div>
       </div>
-      
     </section>
-  )
+  );
 }
