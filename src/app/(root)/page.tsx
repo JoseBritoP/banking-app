e5cb3a0d3d9f53eded773d87e15561ee9f805d06
José Brito <josealbritose@gmail.com>
2024-09-20 17:28:10 -0300

    Main routes

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import HeaderBox from "@/components/shared/HeaderBox";
+import TotalBalanceBox from "@/components/shared/TotalBalanceBox";
+import React from "react";
+
+export default function HomePage() {
+  const loggedIn = { firstName: "Adrian" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext={"Access and manage your account and transactions efficiently"}
+          />
+          
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
+          />
+        </header>
+      </div>
+    </section>
+  );
+}
+
+
