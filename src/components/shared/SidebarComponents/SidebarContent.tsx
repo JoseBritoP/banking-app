6550e5100ffd85f307418f2ee42c1488a8f3335b
José Brito <josealbritose@gmail.com>
2024-09-21 16:39:13 -0300

    Sidebar components

---
+++ src/components/shared/SidebarComponents/SidebarContent.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from '@/public/icons/logo.svg'
+import SidebarLinks from './SidebarLinks'
+import { SheetClose } from '@/components/ui/sheet'
+
+export default function SidebarContent({ user,mobile }: SiderbarProps) {
+  return (
+    <nav className="flex flex-col gap-4">
+      <Link href={"/"} className={`${mobile ? 'mb-0 gap-1 px-4 cursor-pointer items-center' :'mb-12 cursor-pointer items-center gap-2'} `}>
+        <Image
+          src={logo}
+          width={34}
+          height={34}
+          alt="Horizon Logo"
+          className={`${mobile ? '': 'size-[24px] max-xl:size-14' }`}
+        />
+        <h1 className={`${mobile ? 'text-26 font-ibm-plex-serif font-bold text-black-1': 'sidebar-logo'}`}>Horizon</h1>
+      </Link>
+      {mobile && (
+        <div className='mobilenav-sheet'>
+          <SheetClose className='flex h-full flex-col gap-6 pt-16 text-white'>
+            <SidebarLinks mobile={true}/>
+          </SheetClose>
+        </div>
+      )}
+      <SidebarLinks/>
+      USER
+    </nav>
+  )
+}
--- src/components/shared/SidebarComponents/SidebarLinks.tsx
+++ src/components/shared/SidebarComponents/SidebarLinks.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { SheetClose } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
@@ -7,31 +8,51 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-export default function SidebarLinks() {
+export default function SidebarLinks({mobile}:{mobile?:boolean }) {
   // TODO:FIX Image src
   const pathname = usePathname()
   return (
-    <>
+    <div className={mobile ? 'w-full space-y-2' : 'space-y-2'}>
       {sidebarLinks.map((item) => {
         const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
         return (
-          <Link
-            className={cn("sidebar-link",{'bg-bank-gradient':isActive})}
-            href={item.route}
-            key={item.label}
-          >
-            <div className="relative size-6">
-              <Image 
-                src={item.imgURL} alt={item.label} fill
-                className={cn({'brightness-[3] invert-0':isActive})}
-              />
-            </div>
-            <p className={cn('sidebar-label',{
-              '!text-white':isActive
-            })}>{item.label}</p>
-          </Link>
+          <>
+            { mobile === true ? 
+              (<SheetClose asChild key={item.route}>
+                <Link
+                  className={cn("mobilenav-sheet_close w-full border-2 hover:bg-gray-300/30",{'bg-bank-gradient':isActive} )}
+                  href={item.route}
+                  key={item.label}
+                  >
+                    <Image 
+                      src={item.imgURL} alt={item.label} width={30} height={30}
+                      className={cn({'brightness-[3] invert-0':isActive})}
+                    />
+                  <p className={cn('text-16 font-semibold text-black-2',{
+                    'text-white':isActive
+                  })}>{item.label}</p>
+                </Link>
+              </SheetClose>)
+              : 
+              ( <Link
+                className={cn("sidebar-link border-2 hover:bg-gray-300/30",{'bg-bank-gradient':isActive} )}
+                href={item.route}
+                key={item.label}
+                >
+                <div className='relative size-8'>
+                  <Image 
+                    src={item.imgURL} alt={item.label} fill
+                    className={cn({'brightness-[3] invert-0':isActive})}
+                  />
+                </div>
+                <p className={cn('sidebar-label',{
+                  '!text-white':isActive
+                })}>{item.label}</p>
+              </Link>) 
+            }
+          </>
         );
       })}
-    </>
+    </div>
   );
 }
