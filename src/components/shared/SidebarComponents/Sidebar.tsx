94766230df20a70424d9ace6881a87d42ec7ab2f
José Brito <josealbritose@gmail.com>
2024-09-21 16:38:57 -0300

    Sidebar

--- src/components/shared/SidebarComponents/Sidebar.tsx
+++ src/components/shared/SidebarComponents/Sidebar.tsx
@@ -1,27 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import SidebarLinks from "./SidebarLinks";
-import logo from '../../../public/icons/logo.svg'
+import SidebarContent from "./SidebarContent";
 
-export default function SideBar({ user }: SiderbarProps) {
+export default function Sidebar({ user }: SiderbarProps) {
   // TODO: Fix src logo
   return (
     <section className="sidebar">
-      <nav className="flex flex-col gap-4">
-        <Link href={"/"} className="mb-12 curspor-pointer items-center gap-2">
-          <Image
-            src={logo}
-            width={34}
-            height={34}
-            alt="Horizon Logo"
-            className="size-[24px] max-xl:size-14"
-          />
-          <h1 className="sidebar-logo">Horizon</h1>
-        </Link>
-        <SidebarLinks/>
-        USER
-      </nav>
+     <SidebarContent user={user}/>
       FOOTER
     </section>
   );
