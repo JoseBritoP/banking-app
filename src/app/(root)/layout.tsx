eaf212b23c103fe023ea7d7c19380b85a9fa0307
José Brito <josealbritose@gmail.com>
2024-09-21 13:37:44 -0300

    Layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,31 @@
-
+import MobileNav from "@/components/shared/SidebarComponents/MobileNav";
+import Sidebar from "@/components/shared/SidebarComponents/Sidebar";
+import Image from "next/image";
+import mainLogo from '@/public/icons/logo.svg'
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Adrian", lastName: "Invernizzio" };
   return (
     <main className="flex h-screen w-full font-inter">
-      {children}
-      </main>
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src={mainLogo}
+            alt="menu icon"
+            width={30}
+            height={30}
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
