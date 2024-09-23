25f3eca7e474e835f8c66671d2c89e84e5446336
José Brito <josealbritose@gmail.com>
2024-09-23 15:59:59 -0300

    AuthFormFooter

---
+++ src/components/shared/AuthComponents/AuthFormFooter.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function AuthFormFooter({type}:{type:string}) {
+  return (
+    <footer className='flex justify-center gap-1'>
+      <p className='text-14 font-normal text-gray-600'>{type === 'sign-in' ? "Don't have an account?" : "Already have an account?"} </p>
+      <Link href={type === 'sign-in' ? '/sign-un' : 'sign-in'} className='form-link'>{type === 'sign-in' ? 'Sign In' : 'Sign Up'}</Link>
+    </footer>
+  )
+}
