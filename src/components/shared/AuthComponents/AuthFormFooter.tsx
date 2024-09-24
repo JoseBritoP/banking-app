148bfe14c93d39ca958ed1028b7af4a694d20d26
José Brito <josealbritose@gmail.com>
2024-09-24 08:47:43 -0300

    fix error typing

--- src/components/shared/AuthComponents/AuthFormFooter.tsx
+++ src/components/shared/AuthComponents/AuthFormFooter.tsx
@@ -5,7 +5,7 @@ export default function AuthFormFooter({type}:{type:string}) {
   return (
     <footer className='flex justify-center gap-1'>
       <p className='text-14 font-normal text-gray-600'>{type === 'sign-in' ? "Don't have an account?" : "Already have an account?"} </p>
-      <Link href={type === 'sign-in' ? '/sign-un' : 'sign-in'} className='form-link'>{type === 'sign-in' ? 'Sign In' : 'Sign Up'}</Link>
+      <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'} className='form-link'>{type === 'sign-in' ? 'Sign In' : 'Sign Up'}</Link>
     </footer>
   )
 }
