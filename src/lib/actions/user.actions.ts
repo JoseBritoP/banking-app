20f67e58172a3caced4001f5acfdcc38c5ee97c0
José Brito <josealbritose@gmail.com>
2024-09-24 10:33:37 -0300

    user.actions - init

---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Database / Make fetch
+  } catch (error: any) {
+    console.log("Error", error.message);
+  }
+};
+
+export const signUp = async (userData:SignUpParams) => {
+  try {
+    // Create user
+  } catch (error: any) {
+    console.log("Error", error.message);
+  }
+};
