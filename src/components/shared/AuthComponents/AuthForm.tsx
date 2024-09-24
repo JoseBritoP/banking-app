e16aad489c7b0300764c877905bd1644e79b4567
José Brito <josealbritose@gmail.com>
2024-09-24 08:47:32 -0300

    useState isLoading

--- src/components/shared/AuthComponents/AuthForm.tsx
+++ src/components/shared/AuthComponents/AuthForm.tsx
@@ -6,13 +6,23 @@ import AuthFormFooter from "./AuthFormFooter";
 
 export default function AuthForm({ type }: { type: string }) {
   const [user, setUser] = useState(null);
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <section className="auth-form">
-     <AuthHeaderForm type={type} user={user}/>
-      {user ? (<div className="flex flex-col gap-4">{/* PlaidLink */}</div>) : (<AuthFormComponent isLoading={isLoading} setIsLoading={setIsLoading} type={type}/>)}
-      <AuthFormFooter type={type}/>
+      <div className="bg-gray-100/80 rounded-lg pb-4 px-2">
+        <AuthHeaderForm type={type} user={user} />
+        {user ? (
+          <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        ) : (
+          <AuthFormComponent
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
+            type={type}
+          />
+        )}
+        <AuthFormFooter type={type} />
+      </div>
     </section>
   );
 }
