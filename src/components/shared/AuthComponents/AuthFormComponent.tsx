09e1f4f3b4273290dd58cbc24bc57c63668febbf
José Brito <josealbritose@gmail.com>
2024-09-24 08:46:47 -0300

    Submit function init

--- src/components/shared/AuthComponents/AuthFormComponent.tsx
+++ src/components/shared/AuthComponents/AuthFormComponent.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import AuthFormInput from "./AuthFormInput";
 import { authFormSchema } from "@/schema";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface AuthFormComponentProps {
   isLoading: boolean;
@@ -20,23 +21,104 @@ export default function AuthFormComponent({
   isLoading,
   setIsLoading,
 }: AuthFormComponentProps) {
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
     console.log(values);
-    setIsLoading(false);
+    const router = useRouter()
+    try {
+      // Sign In with Appwrite & create plaid token
+      if(type === 'sign-up'){
+        // const newUser = await signUp(values)
+        // setUser(newUser)
+      } 
+
+      if(type === 'sign-in'){
+        // const response = await signIn({
+        //   email:values.email,
+        //   password:values.password
+        // })
+
+        // if(response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally{
+      setIsLoading(false);
+    }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pt-2">
+        {type === "sign-up" && (
+          <>
+            <div className="flex gap-4">
+              <AuthFormInput
+                control={form.control}
+                name="firstName"
+                label="First Name"
+                placeholder="Enter your first name"
+              />
+              <AuthFormInput
+                control={form.control}
+                name="lastName"
+                label="Last Name"
+                placeholder="Enter your last name"
+              />
+            </div>
+            <AuthFormInput
+              control={form.control}
+              name="address"
+              label="Address"
+              placeholder="Enter your specific address"
+            />
+            <AuthFormInput
+              control={form.control}
+              name="city"
+              label="City"
+              placeholder="Enter your specific City"
+            />
+            <div className="flex gap-4">
+              <AuthFormInput
+                control={form.control}
+                name="state"
+                label="State"
+                placeholder="Example: NY"
+              />
+              <AuthFormInput
+                control={form.control}
+                name="postalCode"
+                label="Postal Code"
+                placeholder="Example: 11101"
+              />
+            </div>
+            <div className="flex gap-4">
+              <AuthFormInput
+                control={form.control}
+                name="date"
+                label="Date of Birth"
+                type="date"
+                placeholder="Example: YYYY-MM-DD"
+              />
+              <AuthFormInput
+                control={form.control}
+                name="ssn"
+                label="SSN"
+                placeholder="Example: 1234"
+              />
+            </div>
+          </>
+        )}
         <AuthFormInput
           control={form.control}
           name="email"
