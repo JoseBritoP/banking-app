4ef30421e9fdfd3e582a15b97e9a41fc15b72db2
José Brito <josealbritose@gmail.com>
2024-09-25 14:15:05 -0300

    sign-in and sign-up functions

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,18 +1,58 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
-export const signIn = async () => {
+import { cookies } from "next/headers";
+import { createAdminClient, createSessionClient } from "../server/appwrite";
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+export const signIn = async (userData:signInProps) => {
   try {
-    // Mutation / Database / Make fetch
+    const { account } = await createAdminClient();
+    const response = await account.createEmailPasswordSession(userData.email, userData.password)
+    return parseStringify(response)
   } catch (error: any) {
     console.log("Error", error.message);
   }
 };
 
-export const signUp = async (userData:SignUpParams) => {
+export const signUp = async (userData: SignUpParams) => {
   try {
-    // Create user
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      userData.email,
+      userData.password,
+      `${userData.firstName} ${userData.lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(
+      userData.email,
+      userData.password
+    );
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    console.log(userData)
+
+    return parseStringify(newUserAccount)
   } catch (error: any) {
     console.log("Error", error.message);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error: any) {
+    console.log("Error", error);
+    return null;
+  }
+}
