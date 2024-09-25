eec1bb47fc352ad5f12d23d3db4308f28735550c
José Brito <josealbritose@gmail.com>
2024-09-25 16:32:18 -0300

    HomePage - loggedUser

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from "@/components/shared/HeaderBox";
 import TotalBalanceBox from "@/components/shared/HomeComponents/TotalBalanceBox";
 import RightSidebar from "@/components/shared/SidebarComponents/rigthSidebar/RightSidebar";
+import { getLoggedInUser } from "@/lib/server/appwrite";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function HomePage() {
-  const loggedIn = { firstName: "Adrian", lastName:'Perez',email:'adrian@gmail.com' };
+export default async function HomePage() {
+  const loggedIn = await getLoggedInUser()
+  if(!loggedIn) redirect('/sign-in')
   return (
     <section className="home">
       <div className="home-content">
@@ -12,7 +15,7 @@ export default function HomePage() {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext={"Access and manage your account and transactions efficiently"}
           />
           
